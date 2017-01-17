63cb0c8effc753eb2c88465b24bb9966dad705f8
Doug King <dwking@gmail.com>
2017-01-16 20:52:02 -0800

    cleanup

--- verity-server/spring-boot-server/springServer/src/main/java/site/verity/common/spring/CommonWebConfig.java
+++ verity-server/spring-boot-server/springServer/src/main/java/site/verity/common/spring/CommonWebConfig.java
@@ -2,7 +2,6 @@ package site.verity.common.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan({ "site.verity.web.error" })
